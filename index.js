95c93544539d733f513d9d8a06f892cff2c1a92a
wareethan468-ux <wareethan468-ux@users.noreply.github.com>
2026-08-16 12:18:00 -0500

    Keep repository source only

--- index.js
+++
@@ -1,4 +0,0 @@
-// Apollo/Sparked Host entrypoint.
-// The application code lives in src/index.js; this wrapper lets the panel use
-// its default Startup File value of index.js.
-import './src/index.js';
